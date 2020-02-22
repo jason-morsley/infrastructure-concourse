7d84c22844369c368d4cb0e4cc360953dcbc0b74
Jason Morsley <github@jasonmorsley.dev>
2020-02-22 00:38:27 +0000

    Provsioned SSH keys

---
+++ aws/variables.tf
@@ -0,0 +1,27 @@
+###############################################################################
+# VARIABLES
+###############################################################################
+
+variable "region" {
+  default = "eu-west-2" # London
+}
+
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "key_name" {
+  default = "jason-morsley-dev-concourse"
+}
+
+variable "public_keys_bucket" {
+  default = "jason-morsley-dev-concourse-public-keys"
+}
+
+variable "private_keys_bucket" {
+  default = "jason-morsley-dev-concourse-private-keys"
+}
+
+//variable "keys_bucket_name" {
+//  
+//}
